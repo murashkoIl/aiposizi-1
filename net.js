ff3112da89d137f055366e490b4427258a7e7f29
Dmitry <d.liapkin@gmail.com>
2022-05-04 14:29:44 +0300

    Added OK code response

--- net.js
+++ net.js
@@ -84,7 +84,46 @@ let server = net.createServer((con) => {
                 return;
             }
             let type = mime[path.extname(file).slice(1)] || 'text/plain';
-
+			
+			let s = fs.readFile(file, (err, data) => {
+                if (err) {
+                    let body;
+                    if(url_path.length !== 0) {
+                        file_path = dir + '/';
+                        url_path.forEach((item, index, array) => {
+                            file_path += item;
+                            file_path += '/';
+                        });
+                        file_path = file_path.substring(0, file_path.length - 1);
+                        console.log('visited url: ', url);
+                        fs.appendFileSync(__dirname + '/logs.txt', 'visited url: ' + url + '\n');
+                        fs.readdir(file_path, (err, items) => {
+                            
+                            body = '';
+                            if(items){
+                                items.forEach((item) => {
+                                    if(url === '/'){
+                                        body += '<a href="/' + item + '">' + item + '</a><br />';
+                                    }
+                                    else {
+                                        body += '<a href="' + url + '/' + item + '">' + item + '</a><br />';
+                                    }
+                                    
+                                });
+                                con.write('HTTP/1.1 200 OK\n');
+                                con.write('Access-Control-Allow-Origin: *\n');
+                                con.write('Access-Control-Allow-Methods: ' + methods.join(', ') + '\n');
+                                con.write('Content-Type: text/html\n');
+                                con.write('Content-Length: ' + body.length + '\n\n');
+                                con.write(body);
+                                con.destroy();
+                                fs.appendFileSync(__dirname + '/logs.txt', url + ' contents is shown\n');
+                                console.log('directory contents is shown');
+                            }
+						}
+					}
+				}
+			}
         }
     });
 });
